fc16530bc3bb7353bcff739c3cb0cc96d8b1cfd4
Cesar Aguilar <Blueavian9@gmail.com>
2025-03-14 10:05:08 -0700

    updated files

--- test-runner.mjs
+++ test-runner.mjs
@@ -1,55 +1,55 @@
-const analyser = require("./assertion-analyser");
-const EventEmitter = require("events").EventEmitter;
+import analyser from "./assertion-analyser";
+import { EventEmitter } from "events";
+import Mocha from "mocha";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-const Mocha = require("mocha"),
-  fs = require("fs"),
-  path = require("path");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const mocha = new Mocha();
-const testDir = "./tests";
+const testDir = path.join(__dirname, "./tests");
 
 // Add each .js file to the mocha instance
 fs.readdirSync(testDir)
-  .filter(function (file) {
-    // Only keep the .js files
-    return file.substr(-3) === ".js";
-  })
-  .forEach(function (file) {
+  .filter((file) => file.endsWith(".js"))
+  .forEach((file) => {
     mocha.addFile(path.join(testDir, file));
   });
 
 const emitter = new EventEmitter();
+
 emitter.run = function () {
   const tests = [];
   let context = "";
   const separator = " -> ";
-  // Run the tests.
+
   try {
     const runner = mocha
       .ui("tdd")
       .run()
-      .on("test end", function (test) {
-        // remove comments
-        let body = test.body.replace(/\/\/.*\n|\/\*.*\*\//g, "");
-        // collapse spaces
-        body = body.replace(/\s+/g, " ");
+      .on("test end", (test) => {
+        let body = test.body.replace(/\/\/.*\n|\/\*.*\*\//g, ""); // remove comments
+        body = body.replace(/\s+/g, " "); // collapse spaces
+
         const obj = {
           title: test.title,
           context: context.slice(0, -separator.length),
           state: test.state,
-          // body: body,
           assertions: analyser(body),
         };
         tests.push(obj);
       })
-      .on("end", function () {
+      .on("end", () => {
         emitter.report = tests;
         emitter.emit("done", tests);
       })
-      .on("suite", function (s) {
+      .on("suite", (s) => {
         context += s.title + separator;
       })
-      .on("suite end", function (s) {
+      .on("suite end", (s) => {
         context = context.slice(0, -(s.title.length + separator.length));
       });
   } catch (e) {
@@ -57,21 +57,4 @@ emitter.run = function () {
   }
 };
 
-module.exports = emitter;
-
-/*
- * Mocha.runner Events:
- * can be used to build a better custom report
- *
- *   - `start`  execution started
- *   - `end`  execution complete
- *   - `suite`  (suite) test suite execution started
- *   - `suite end`  (suite) all tests (and sub-suites) have finished
- *   - `test`  (test) test execution started
- *   - `test end`  (test) test completed
- *   - `hook`  (hook) hook execution started
- *   - `hook end`  (hook) hook complete
- *   - `pass`  (test) test passed
- *   - `fail`  (test, err) test failed
- *   - `pending`  (test) test pending
- */
+export default emitter;
